fda99395a788581a1e50f4eb2eea379edc800a65
Michal Bienias <michalbienias9@gmail.com>
2023-11-28 13:15:41 -0500

    Init Com

---
+++ example.js
@@ -0,0 +1,32 @@
+const apiKey = "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";
+
+const submitIcon = document.querySelector("#submit-icon")
+const inputElement = document.querySelector("input")
+
+
+const getImages = async() => {
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${apiKey}`,
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({
+      "prompt": inputElement.value,
+      "n": 1,
+      "size": "1024x1024"
+    })
+  }
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/images/generations', options)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+submitIcon.addEventListener('click', getImages)
